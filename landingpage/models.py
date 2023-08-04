45d201866de4ca5bbd2556c35d68792588556a93
Christian-Migliore <138627281+Christian-Migliore@users.noreply.github.com>
2023-08-04 10:55:25 +0200

    Delete landingpage directory

--- landingpage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class LandingPage(models.Model):
-    message = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.message
